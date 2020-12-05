99aa1b7f168a9a61d6774cd3e1155172adc4de7f
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-05 05:01:49 +0300

    Scripted auto-commit on change (2020-12-05 05:01:49) by gitwatch.sh

--- src/main/java/StoreActor.java
+++ src/main/java/StoreActor.java
@@ -3,5 +3,19 @@ import akka.actor.AbstractActor;
 import java.util.HashMap;
 
 public class StoreActor extends AbstractActor {
-    private HashMap<> storage;
+    private HashMap<Test, Long> storage;
+
+    public StoreActor() {
+        this.storage = new HashMap<>();
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(Test.class,this::getTest)
+    }
+
+    private void getTest(Test testURL) {
+        getSender().tell();
+    }
 }
