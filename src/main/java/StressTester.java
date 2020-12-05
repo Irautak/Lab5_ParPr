041f8153ab37a9da0c040754430733cdc495990e
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-05 04:54:51 +0300

    Scripted auto-commit on change (2020-12-05 04:54:51) by gitwatch.sh

--- src/main/java/StressTester.java
+++ src/main/java/StressTester.java
@@ -1,2 +1,26 @@
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import org.asynchttpclient.AsyncHttpClient;
+
+import java.net.http.HttpRequest;
+
 public class StressTester {
+    private ActorRef actorRef;
+    private ActorMaterializer materializer;
+    private AsyncHttpClient asyncHttpClient;
+
+    public StressTester(AsyncHttpClient asyncHttpClient, ActorSystem system, ActorMaterializer materializer) {
+        this.actorRef = system.actorOf(Props.create(**))
+        this.materializer = materializer;
+        this.asyncHttpClient = asyncHttpClient;
+    }
+
+    public Flow<HttpRequest, HttpResponse, NotUsed> createRoute() {
+
+    }
 }
