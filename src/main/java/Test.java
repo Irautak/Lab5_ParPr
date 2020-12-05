1a242c8de1f2ef7834b30e7be99f7e8446bb1b34
Vlad Lomtev <vladlomtev@gmail.com>
2020-12-05 04:58:14 +0300

    Scripted auto-commit on change (2020-12-05 04:58:14) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,2 +1,19 @@
-public class Test {
+import java.io.Serializable;
+
+public class Test implements Serializable {
+    private String url;
+    private Integer count;
+
+    public Test(String url, Integer count) {
+        this.url = url;
+        this.count = count;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public String getUrl() {
+        return url;
+    }
 }
